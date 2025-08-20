60c9169e4ad0491180e60c671495760598a9de82
udit jain <uditj66@gmail.com>
2025-08-21 01:17:44 +0530

    first commit

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,7 +3,7 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
+    { title: "ai -resume -analyzer" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
